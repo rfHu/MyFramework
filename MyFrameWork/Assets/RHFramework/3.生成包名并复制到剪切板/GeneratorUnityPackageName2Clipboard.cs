32b28ee5e4dbb9cbf796b059999df25def6a3f97
29BA6YJUZ4M874I\Administrator <2449663340@qq.com>
2019-01-18 16:34:21 +0800

    保存生成包名到剪切板

---
+++ MyFrameWork/Assets/RHFramework/3.生成包名并复制到剪切板/GeneratorUnityPackageName2Clipboard.cs
@@ -0,0 +1,19 @@
+﻿#if UNITY_EDITOR
+using System;
+using UnityEditor;
+using UnityEngine;
+#endif
+
+namespace RHFramework
+{
+    public class GeneratorUnityPackageName2Clipboard
+    {
+#if UNITY_EDITOR
+        [MenuItem("RHFramework/3.生成文件名到剪切板")]
+#endif
+        private static void MenuClicked()
+        {
+            GUIUtility.systemCopyBuffer = "RHFramework_" + DateTime.Now.ToString("yyyyMMdd_HH");
+        }
+    }
+}
