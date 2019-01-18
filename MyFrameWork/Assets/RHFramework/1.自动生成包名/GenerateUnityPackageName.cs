f3ca2b6631c074c96e26fe7938c09dee314d9a4a
29BA6YJUZ4M874I\Administrator <2449663340@qq.com>
2019-01-18 15:11:52 +0800

    自动生成打包名

---
+++ MyFrameWork/Assets/RHFramework/1.自动生成包名/GenerateUnityPackageName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GenerateUnityPackageName {
+
+    [MenuItem("RHFramework/1.自动生成package名字")]
+    public static void MenuClicked()
+    {
+        Debug.Log("RHFramework_" + DateTime.Now.ToString("yyyyMMdd_HH"));
+    }
+}
