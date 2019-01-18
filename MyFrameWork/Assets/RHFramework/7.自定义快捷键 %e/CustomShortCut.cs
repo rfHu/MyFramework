06c802e09990159896807d3d75b90fe2c5615dfa
29BA6YJUZ4M874I\Administrator <2449663340@qq.com>
2019-01-18 18:35:15 +0800

    自定义快捷键

---
+++ MyFrameWork/Assets/RHFramework/7.自定义快捷键 %e/CustomShortCut.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace RHFramework
+{
+    public class CustomShortCut
+    {
+#if UNITY_EDITOR
+        [MenuItem("RHFramework/7.创建快捷键 %e")]
+        private static void MenuClicked()
+        {
+            EditorApplication.ExecuteMenuItem("RHFramework/6.MenuItem复用");
+        }
+#endif
+    }
+}
