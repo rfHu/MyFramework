601857e183de09756677dfb1c335c8fd79f43eae
29BA6YJUZ4M874I\Administrator <2449663340@qq.com>
2019-01-21 15:39:08 +0800

    transform 数据重置

---
+++ MyFrameWork/Assets/RHFramework/11.transform 重置/TransformIdentity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace RHFramework
+{
+    public class TransformIdentity
+    {
+
+        private static void Identity(Transform transform)
+        {
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = Quaternion.identity;
+            transform.localScale = Vector3.one;
+        }
+
+#if UNITY_EDITOR
+        [MenuItem("RHFramework/11.Transform 重置")]
+        private static void MenuClicked()
+        {
+            GameObject gameObject = new GameObject();
+
+            TransformLocalPosSimplify.SetLocalPosX(gameObject.transform, 5.5f);
+
+            Identity(gameObject.transform);
+        }
+#endif
+    }
+}
