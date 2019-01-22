40ab613b5070de54f5060c8d395a2b4cd5ae73d7
29BA6YJUZ4M874I\Administrator <2449663340@qq.com>
2019-01-22 19:32:15 +0800

    消息机制完善

--- MyFrameWork/Assets/RHFramework/12.简单消息机制/MsgDispatcher.cs
+++ MyFrameWork/Assets/RHFramework/12.简单消息机制/MsgDispatcher.cs
@@ -11,30 +11,52 @@ namespace RHFramework
 
         public static void Register(string msgName, Action<object> onMsgReceived)
         {
-            RegisteredMsgs.Add(msgName, onMsgReceived);
+            if (!RegisteredMsgs.ContainsKey(msgName))
+            {
+                RegisteredMsgs.Add(msgName, _ => { });
+            }
+            RegisteredMsgs[msgName] += onMsgReceived;
         }
 
-        public static void UnRegister(string MsgName)
+        public static void UnRegisterAll(string msgName)
         {
-            RegisteredMsgs.Remove(MsgName);
+            RegisteredMsgs.Remove(msgName);
+        }
+
+        public static void UnRegister(string msgName, Action<object> onMsgReceived)
+        {
+            if (RegisteredMsgs.ContainsKey(msgName))
+            {
+                RegisteredMsgs[msgName] -= onMsgReceived;
+            }
+            
         }
 
         public static void Send(string msgName, object data)
         {
-            RegisteredMsgs[msgName](data);
+            if (RegisteredMsgs.ContainsKey(msgName))
+            {
+                RegisteredMsgs[msgName](data);
+            }
         }
 
 #if UNITY_EDITOR
         [UnityEditor.MenuItem("RHFramework/12.简易消息机制", false, 12)]
         private static void MenuClicked()
         {
-            Register("MSG1", data => { Debug.LogFormat("MSG1:{0}", data); });
+            Register("MSG1", OnMsgReceived);
+            Register("MSG1", OnMsgReceived);
 
             Send("MSG1", "hello world");
 
-            UnRegister("MSG1");
+            UnRegister("MSG1", OnMsgReceived);
 
-            Send("MSG1", "hello world");
+            Send("MSG1", "hello");
+        }
+
+        private static void OnMsgReceived(object data)
+        {
+            Debug.LogFormat("MSG1:{0}", data);
         }
 #endif
     }
