eba05c1e77c5a53ac8bd3d0bdce99f864a591015
29BA6YJUZ4M874I\Administrator <2449663340@qq.com>
2019-01-22 19:17:04 +0800

    计时功能， 简易消息机制

---
+++ MyFrameWork/Assets/RHFramework/12.简单消息机制/MsgDispatcher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RHFramework
+{
+    public class MsgDispatcher
+    {
+        private static Dictionary<string, Action<object>> RegisteredMsgs = new Dictionary<string, Action<object>>();
+
+        public static void Register(string msgName, Action<object> onMsgReceived)
+        {
+            RegisteredMsgs.Add(msgName, onMsgReceived);
+        }
+
+        public static void UnRegister(string MsgName)
+        {
+            RegisteredMsgs.Remove(MsgName);
+        }
+
+        public static void Send(string msgName, object data)
+        {
+            RegisteredMsgs[msgName](data);
+        }
+
+#if UNITY_EDITOR
+        [UnityEditor.MenuItem("RHFramework/12.简易消息机制", false, 12)]
+        private static void MenuClicked()
+        {
+            Register("MSG1", data => { Debug.LogFormat("MSG1:{0}", data); });
+
+            Send("MSG1", "hello world");
+
+            UnRegister("MSG1");
+
+            Send("MSG1", "hello world");
+        }
+#endif
+    }
+}
