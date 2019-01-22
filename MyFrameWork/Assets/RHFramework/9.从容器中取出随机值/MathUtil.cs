c515155d471e00ca59e359a5cc2e49526c4d2a13
29BA6YJUZ4M874I\Administrator <2449663340@qq.com>
2019-01-22 16:40:34 +0800

    从容器中取出随机值

---
+++ MyFrameWork/Assets/RHFramework/9.从容器中取出随机值/MathUtil.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RHFramework
+{
+    public partial class MathUtil
+    {
+        public static T GetRandomValueFrom<T>(params T[] values)
+        {
+            return values[Random.Range(0, values.Length)];
+        }
+
+
+#if UNITY_EDITOR
+        [UnityEditor.MenuItem("RHFramework/9.从若干值中取一个随机值",false , 9)]
+        private static void MenuClicked2()
+        {
+            Debug.Log(GetRandomValueFrom(1, 2, 3));
+            Debug.Log(GetRandomValueFrom("ssss", "dfds"));
+            Debug.Log(GetRandomValueFrom(5.66, 3.4, 77.56));
+        }
+#endif
+    }
+}
