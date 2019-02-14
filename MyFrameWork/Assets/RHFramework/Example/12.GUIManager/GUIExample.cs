41597c59b37e1960ebdb0e924088fe735c32bc37
29BA6YJUZ4M874I\Administrator <2449663340@qq.com>
2019-02-14 18:38:05 +0800

    GUIManager加载功能

---
+++ MyFrameWork/Assets/RHFramework/Example/12.GUIManager/GUIExample.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RHFramework
+{
+    public class GUIExample : MonoBehaviour
+    {
+        void Start()
+        {
+            GUIManager.LoadPanel("HomePanel");
+        }
+    }
+}
---
+++ MyFrameWork/Assets/RHFramework/Framework/Manager/GUIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RHFramework
+{
+    public class GUIManager 
+    {
+        public static void LoadPanel(string panelName)
+        {
+            var canvasObj = GameObject.Find("Canvas");
+
+            var homePanelPrefab = Resources.Load<GameObject>(panelName);
+            var homePanelObj = Object.Instantiate(homePanelPrefab);
+
+            homePanelObj.transform.SetParent(canvasObj.transform);
+
+            var homeRectTrans = homePanelObj.transform as RectTransform;
+
+            homeRectTrans.offsetMin = Vector2.zero;
+            homeRectTrans.offsetMax = Vector2.zero;
+            homeRectTrans.anchoredPosition3D = Vector3.zero;
+            homeRectTrans.anchorMin = Vector2.zero;
+            homeRectTrans.anchorMax = Vector2.one;
+        }
+    }
+}
