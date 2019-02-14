74318f063dc3c8dd39b406eb846df54e2ee9d9d0
29BA6YJUZ4M874I\Administrator <2449663340@qq.com>
2019-02-14 16:32:47 +0800

    升级unity ，实现MainManager的开发状态控制，并编写示例

---
+++ MyFrameWork/Assets/RHFramework/Example/11.MainManager/Home/HomeModule.cs
@@ -0,0 +1,30 @@
+﻿using RHFramework;
+
+namespace Game
+{
+    public class HomeModule : MainManager
+    {
+        protected override void LaunchInDevelopingMode()
+        {
+            //开发逻辑
+        }
+
+        protected override void LaunchInTestMode()
+        {
+            //测试逻辑
+            //加载资源
+            //初始化 SDK
+            //点击开始游戏
+            GameModule.LoadModule();
+        }
+
+        protected override void LaunchInProductionMode()
+        {
+            //生产逻辑
+            //加载资源
+            //初始化 SDK
+            //点击开始游戏
+            GameModule.LoadModule();
+        }
+    }
+}
---
+++ MyFrameWork/Assets/RHFramework/Framework/Manager/MainManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RHFramework
+{
+    public enum EnvironmentMode
+    {
+        Developing,
+        Test,
+        Production
+    }
+
+    public abstract class MainManager : MonoBehaviour
+    {
+        public EnvironmentMode Mode;
+
+        private static EnvironmentMode mSharedMode;
+        private static bool mModeSetted = false;
+
+        void Start()
+        {
+            if (!mModeSetted)
+            {
+                mSharedMode = Mode;
+                mModeSetted = true;
+            }
+
+            switch (mSharedMode)
+            {
+                case EnvironmentMode.Developing:
+                    LaunchInDevelopingMode();
+                    break;
+                case EnvironmentMode.Test:
+                    LaunchInTestMode();
+                    break;
+                case EnvironmentMode.Production:
+                    LaunchInProductionMode();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        protected abstract void LaunchInDevelopingMode();
+        
+        protected abstract void LaunchInTestMode();
+
+        protected abstract void LaunchInProductionMode();
+    }
+}
