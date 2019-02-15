c3fc35a3d753fff82f65eafa87324d77b79e94d2
RaineHuCompany <2449663340@qq.com>
2019-02-15 19:05:44 +0800

    简单对象池

---
+++ MyFrameWork/Assets/RHFramework/Framework/Util/SimpleObjectPool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace RHFramework
+{
+    public interface IPool<T>
+    {
+        T Allocate();
+
+        bool Recycle(T obj);
+    }
+
+    public interface IObjectFactory<T>
+    {
+        T Create();
+    }
+
+    public abstract class Pool<T>: IPool<T>
+    {
+        #region ICountObserverable
+        /// <summary>
+        /// Gets the current count.
+        /// </summary>
+        /// <value>The current count.</value>
+        public int CurCount
+        {
+            get { return mCachedStack.Count; }
+        }
+        #endregion
+
+        protected IObjectFactory<T> mFactory;
+
+        Stack<T> mCachedStack = new Stack<T>();
+
+        /// <summary>
+        /// default is 5
+        /// </summary>
+        protected int mMaxCount = 5;
+
+        public virtual T Allocate()
+        {
+            return mCachedStack.Count == 0 ? mFactory.Create() : mCachedStack.Pop();
+        }
+
+        public abstract bool Recycle(T obj);
+    }
+}
