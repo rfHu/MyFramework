162889baba9b0b46dc87cb372bc5f24c426a7f2f
RaineHuCompany <2449663340@qq.com>
2019-02-19 19:14:12 +0800

    LevelManager 实现 V0.0.3

---
+++ MyFrameWork/Assets/RHFramework/Framework/Manager/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RHFramework
+{
+    public class LevelManager
+    {
+        private static List<string> mLevelNames;
+
+        public static int Index { get; set; }
+
+        public static void Init(List<string> levelNames)
+        {
+            Index = 0;
+            mLevelNames = levelNames;
+        }
+
+        public static void LoadCurrent()
+        {
+            SceneManager.LoadScene(mLevelNames[Index]);
+        }
+
+        public static void LoadNext()
+        {
+            Index++;
+
+            //容错
+            if (Index > mLevelNames.Count)
+            {
+                Index = 0;
+            }
+
+            SceneManager.LoadScene(mLevelNames[Index]);
+        }
+    }
+}
