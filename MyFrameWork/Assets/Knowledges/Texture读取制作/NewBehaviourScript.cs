7cb1088daa8c686c407191810091840c6bca8522
RaineHuCompany <2449663340@qq.com>
2019-02-28 19:11:56 +0800

    拼花和波导线图片处理

--- MyFrameWork/Assets/Knowledges/Texture读取制作/NewBehaviourScript.cs
+++ MyFrameWork/Assets/Knowledges/Texture读取制作/NewBehaviourScript.cs
@@ -1,9 +1,40 @@
 ﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.IO;
 using UnityEngine;
 
 namespace RHFramework {
     public class NewBehaviourScript : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        [UnityEditor.MenuItem("MyKnowledge/1_", false, 1)]
+#endif
+        private static void MenuClicked()
+        {
+            var texture2D1 = Resources.Load<Texture2D>("28");
+            var texture2D2 = Resources.Load<Texture2D>("28_1");
+
+            //DrawIcon(new Texture2D[] { texture2D1, texture2D2 }, new Texture2D[] { pic1, pic2, pic3, pic4, pic5 });
+            //RGBImageUtil.DrawPartPics(new Texture2D[] { texture2D1, texture2D2 });
+            RGBImageUtil.DrawPartImg(new Bitmap[] 
+            {
+                new Bitmap(@"E:\STClone\MyFramework\MyFrameWork\Assets\Knowledges\Texture读取制作\Resources\28.png"),
+                new Bitmap(@"E:\STClone\MyFramework\MyFrameWork\Assets\Knowledges\Texture读取制作\Resources\28_1.png")
+            });
+
+            texture2D1 = texture2D2 = null;
+
+            DestroyImmediate(texture2D1, true);
+            DestroyImmediate(texture2D2, true);
+
+            Resources.UnloadUnusedAssets();
+
+        }
+
+    }
+
+    public static class RGBImageUtil
     {
         public static Texture2D DrawIcon(Texture2D[] RGBtextures, Texture2D[] textures, int finalWidth = 100, int finalHeight = 100)
         {
@@ -23,9 +54,9 @@ namespace RHFramework {
 
                     for (int i = 0; i < RGBtextures.Length; i++)
                     {
-                        Color tempColor = RGBtextures[i].GetPixel(x, y);
+                        UnityEngine.Color tempColor = RGBtextures[i].GetPixel(x, y);
 
-                        if (tempColor == Color.black)
+                        if (tempColor == UnityEngine.Color.black)
                         {
                             continue;
                         }
@@ -46,7 +77,7 @@ namespace RHFramework {
                         }
                     }
 
-                    var aimColor = picLayerNum == -1 ? Color.clear : textures[picLayerNum].GetPixel(x, y);
+                    var aimColor = picLayerNum == -1 ? UnityEngine.Color.clear : textures[picLayerNum].GetPixel(x, y);
 
                     newTexture.SetPixel(x, y, aimColor);
                 }
@@ -54,7 +85,7 @@ namespace RHFramework {
 
             newTexture.Apply();
 
-            newTexture = ScaleTexture(newTexture, finalWidth, finalHeight);
+            newTexture = TextureUtil.ScaleTexture(newTexture, finalWidth, finalHeight);
 
             SaveTexture2PNG(newTexture, Application.dataPath + "/" + "tempPng" + ".png");
 
@@ -75,140 +106,173 @@ namespace RHFramework {
             texture = null;
         }
 
-        /// <summary>
-        /// 缩放Texture2D
-        /// </summary>
-        /// <param name="source">目标Texture2D</param>
-        /// <param name="targetWidth">目标宽</param>
-        /// <param name="targetHeight">目标高</param>
-        /// <returns>缩放后的Texture2D</returns>
-        public static Texture2D ScaleTexture(Texture2D source, int targetWidth, int targetHeight)
-        {
-            Texture2D result = new Texture2D(targetWidth, targetHeight, source.format, false);
+        //public static void DrawPartPics(Texture2D[] RGBtextures, int finalWidth = 280, int finalHeight = 280)
+        //{
+        //    //获得新图宽高
+        //    var width = RGBtextures[0].width;
+        //    var height = RGBtextures[0].height;
 
-            for (int i = 0; i < result.height; ++i)
-            {
-                for (int j = 0; j < result.width; ++j)
-                {
-                    Color newColor = source.GetPixelBilinear((float)j / (float)result.width, (float)i / (float)result.height);
-                    result.SetPixel(j, i, newColor);
-                }
-            }
+        //    List<Texture2D> textures = new List<Texture2D>();
 
-            result.Apply();
-            return result;
-        }
+        //    //计算像素
+        //    for (int y = 0; y < height; y++)
+        //    {
+        //        for (int x = 0; x < width; x++)
+        //        {
+        //            var picLayerNum = -1;
 
-        public static Texture2D DrawTexture2DBG(Texture2D texture2D, Color color)
-        {
-            var colors = new Color[texture2D.width * texture2D.height];
+        //            for (int i = 0; i < RGBtextures.Length; i++)
+        //            {
+        //                Color tempColor = RGBtextures[i].GetPixel(x, y);
 
-            for (int i = 0; i < colors.Length; i++)
-            {
-                colors[i] = color;
-            }
+        //                if (tempColor == Color.black)
+        //                {
+        //                    continue;
+        //                }
+        //                else
+        //                {
+        //                    if (tempColor.r != 0)
+        //                    {
+        //                        picLayerNum = i * 3 + 2;
+        //                    }
+        //                    else if (tempColor.g != 0)
+        //                    {
+        //                        picLayerNum = i * 3 + 1;
+        //                    }
+        //                    else if (tempColor.b != 0)
+        //                    {
+        //                        picLayerNum = i * 3;
+        //                    }
+        //                }
+        //            }
 
-            texture2D.SetPixels(colors);
+        //            if (picLayerNum != -1)
+        //            {
+        //                while (picLayerNum + 1 > textures.Count)
+        //                {
+        //                    var texture2D = new Texture2D(width, height);
+        //                    texture2D = TextureUtil.DrawTexture2DColor(texture2D, Color.clear);
+        //                    textures.Add(texture2D);
+        //                }
 
-            texture2D.Apply();
-            return texture2D;
-        }
+        //                textures[picLayerNum].SetPixel(x, y, new Color(0.4f, 0.4f, 0.4f));
+        //            }
+        //        }
+        //    }
 
-        public static void DrawPartPics(Texture2D[] RGBtextures, int finalWidth = 280, int finalHeight = 280)
+        //    for (int i = 0; i < textures.Count; i++)
+        //    {
+        //        textures[i] = TextureUtil.ScaleTexture(textures[i], finalWidth, finalHeight);
+
+        //        SaveTexture2PNG(textures[i], Application.dataPath + "/" + i + ".png");
+
+        //        textures[i] = null;
+        //    }
+
+        //    Debug.Log("finish");
+        //}
+
+        public static void DrawPartImg(Bitmap[] RGBImages, int finalWidth = 280, int finalHeight = 280)
         {
             //获得新图宽高
-            var width = RGBtextures[0].width;
-            var height = RGBtextures[0].height;
+            var width = RGBImages[0].Width;
+            var height = RGBImages[0].Height;
 
-            List<Texture2D> textures = new List<Texture2D>();
+            List<Bitmap> images = new List<Bitmap>();
 
             //计算像素
             for (int y = 0; y < height; y++)
             {
                 for (int x = 0; x < width; x++)
                 {
-                    var picLayerNum = -1;
+                    var picLayerNum = 0;
 
-                    for (int i = 0; i < RGBtextures.Length; i++)
+                    for (int i = 0; i < RGBImages.Length; i++)
                     {
-                        Color tempColor = RGBtextures[i].GetPixel(x, y);
+                        System.Drawing.Color tempColor = RGBImages[i].GetPixel(x, y);
 
-                        if (tempColor == Color.black)
+                        if (tempColor == System.Drawing.Color.Black)
                         {
                             continue;
                         }
                         else
                         {
-                            if (tempColor.r != 0)
+                            if (tempColor.R != 0)
+                            {
+                                picLayerNum = i * 3 + 3;
+                            }
+                            else if (tempColor.G != 0)
                             {
                                 picLayerNum = i * 3 + 2;
                             }
-                            else if (tempColor.g != 0)
+                            else if (tempColor.B != 0)
                             {
                                 picLayerNum = i * 3 + 1;
-                            }
-                            else if (tempColor.b != 0)
-                            {
-                                picLayerNum = i * 3;
                             }
                         }
                     }
 
                     if (picLayerNum != -1)
                     {
-                        while (picLayerNum + 1 > textures.Count)
+                        while (picLayerNum + 1 > images.Count)
                         {
-                            var texture2D = new Texture2D(width, height);
-                            texture2D = DrawTexture2DBG(texture2D, new Color(0.94f, 0.94f, 0.94f));
-                            textures.Add(texture2D);
+                            var image = new Bitmap(width, height);
+                            image = DrawWgoleImageColor(image, System.Drawing.Color.FromArgb(0,0,0,0));
+                            images.Add(image);
                         }
 
-                        textures[picLayerNum].SetPixel(x, y, new Color(0.4f, 0.4f, 0.4f));
+                        images[picLayerNum].SetPixel(x, y, ColorTranslator.FromHtml("#ffffff"));
                     }
                 }
             }
 
-            for (int i = 0; i < textures.Count; i++)
+            for (int i = 0; i < images.Count; i++)
             {
-                textures[i] = ScaleTexture(textures[i], finalWidth, finalHeight);
+                images[i].ScaleToSize(finalWidth, finalHeight);
 
-                SaveTexture2PNG(textures[i], Application.dataPath + "/" + i + ".png");
+                SaveImagePNG(images[i], Application.dataPath + "/" + i + ".png");
 
-                textures[i] = null;
+                images[i].Dispose();
             }
 
             Debug.Log("finish");
         }
-#if UNITY_EDITOR
-        [UnityEditor.MenuItem("MyKnowledge/1_", false, 1)]
-#endif
-        private static void MenuClicked()
+
+        public static Bitmap DrawWgoleImageColor(Bitmap image, System.Drawing.Color color)
         {
-            var texture2D1 = Resources.Load<Texture2D>("28");
-            var texture2D2 = Resources.Load<Texture2D>("28_1");
-            var pic1 = Resources.Load<Texture2D>("pic3");
-            var pic2 = Resources.Load<Texture2D>("pic2");
-            var pic3 = Resources.Load<Texture2D>("pic1");
-            var pic4 = Resources.Load<Texture2D>("pic5");
-            var pic5 = Resources.Load<Texture2D>("pic4");
+            var colors = new System.Drawing.Color[image.Width * image.Height];
 
-            //DrawIcon(new Texture2D[] { texture2D1, texture2D2 }, new Texture2D[] { pic1, pic2, pic3, pic4, pic5 });
-            DrawPartPics(new Texture2D[] { texture2D1, texture2D2 });
-
-            texture2D1 = texture2D2 = null;
-            pic1 = pic2 = pic3 = pic4 = pic5 = null;
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    image.SetPixel(i,j,color);
+                }
+            }
             
-            DestroyImmediate(texture2D1, true);
-            DestroyImmediate(texture2D2, true);
-            DestroyImmediate(pic1, true);
-            DestroyImmediate(pic2, true);
-            DestroyImmediate(pic3, true);
-            DestroyImmediate(pic4, true);
-            DestroyImmediate(pic5, true);
-
-            Resources.UnloadUnusedAssets();
-
+            return image;
         }
 
+        public static Bitmap ScaleToSize(this Bitmap bitmap, int width, int height)
+        {
+            if (bitmap.Width == width && bitmap.Height == height)
+            {
+                return bitmap;
+            }
+
+            var scaledBitmap = new Bitmap(width, height);
+            using (var g = System.Drawing.Graphics.FromImage(scaledBitmap))
+            {
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.DrawImage(bitmap, 0, 0, width, height);
+            }
+
+            return scaledBitmap;
+        }
+        
+        private static void SaveImagePNG(Bitmap image, string path)
+        {
+            image.Save(path);
+        }
     }
 }
---
+++ MyFrameWork/Assets/Knowledges/Texture读取制作/Scripts/TextureUtil.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RHFramework
+{
+    public static partial class TextureUtil
+    {
+        /// <summary>
+        /// 缩放Texture2D
+        /// </summary>
+        /// <param name="source">目标Texture2D</param>
+        /// <param name="targetWidth">目标宽</param>
+        /// <param name="targetHeight">目标高</param>
+        /// <returns>缩放后的Texture2D</returns>
+        public static Texture2D ScaleTexture(Texture2D source, int targetWidth, int targetHeight)
+        {
+            Texture2D result = new Texture2D(targetWidth, targetHeight, source.format, false);
+
+            for (int i = 0; i < result.height; ++i)
+            {
+                for (int j = 0; j < result.width; ++j)
+                {
+                    Color newColor = source.GetPixelBilinear((float)j / (float)result.width, (float)i / (float)result.height);
+                    result.SetPixel(j, i, newColor);
+                }
+            }
+
+            result.Apply();
+            return result;
+        }
+
+        public static Texture2D DrawTexture2DColor(Texture2D texture2D, Color color)
+        {
+            var colors = new Color[texture2D.width * texture2D.height];
+
+            for (int i = 0; i < colors.Length; i++)
+            {
+                colors[i] = color;
+            }
+
+            texture2D.SetPixels(colors);
+
+            texture2D.Apply();
+            return texture2D;
+        }
+    }
+}
