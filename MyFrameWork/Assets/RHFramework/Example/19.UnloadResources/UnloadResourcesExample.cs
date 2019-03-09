813d7a2e8eeadb9fad22072467f9c54e2cc4c50b
RaineHuCompany <2449663340@qq.com>
2019-03-09 18:43:10 +0800

    卸载Resources资源实现

---
+++ MyFrameWork/Assets/RHFramework/Example/19.UnloadResources/UnloadResourcesExample.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnloadResourcesExample : MonoBehaviour
+{
+
+#if UNITY_EDITOR
+    [UnityEditor.MenuItem("RHFramework/Example/19.UnloadResourcesExample", false, 19)]
+#endif
+    static void MenuCilcked()
+    {
+        UnityEditor.EditorApplication.isPlaying = true;
+
+        new GameObject().AddComponent<UnloadResourcesExample>();
+    }
+
+    IEnumerator Start()
+    {
+        var coin = Resources.Load("getcoin");
+
+        yield return new WaitForSeconds(5.0f);
+
+        Resources.UnloadAsset(coin);
+        Debug.Log("getcoin Unloaded");
+    }
+}
---
+++ MyFrameWork/Assets/RHFramework/Example/20.UnloadPrefab/UnloadPrefabExample.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnloadPrefabExample : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [UnityEditor.MenuItem("RHFramework/Example/20.UnloadPrefabExample", false, 20)]
+#endif
+    static void MenuCilcked()
+    {
+        UnityEditor.EditorApplication.isPlaying = true;
+
+        new GameObject().AddComponent<UnloadPrefabExample>();
+    }
+
+    IEnumerator Start()
+    {
+        var gamePanel = Resources.Load("GamePanel");
+
+        yield return new WaitForSeconds(5.0f);
+
+        gamePanel = null;
+        
+        Resources.UnloadUnusedAssets();
+        Debug.Log("gamePanel Unloaded");
+    }
+}
