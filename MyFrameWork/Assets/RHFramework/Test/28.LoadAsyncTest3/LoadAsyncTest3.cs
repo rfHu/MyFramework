bd79cb9b4515f723c300a75ea80d46533e4fffbc
RaineHuHome <2449663340@qq.com>
2019-07-17 17:04:21 +0800

    v0.0.13——Res工厂类实现 v0.0.14——示例目录分化迁移

---
+++ MyFrameWork/Assets/RHFramework/Test/28.LoadAsyncTest3/LoadAsyncTest3.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RHFramework {
+    public class LoadAsyncTest3 : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        [UnityEditor.MenuItem("RHFramework/Example/28.LoadAsyncTest3", false, 28)]
+        static void MenuCilcked()
+        {
+            UnityEditor.EditorApplication.isPlaying = true;
+
+            new GameObject("LoadAsyncTest3").AddComponent<LoadAsyncTest3>();
+        }
+#endif
+
+        ResLoader mResLoader = new ResLoader();
+
+        private void Start()
+        {
+            mResLoader.LoadAsync<Texture2D>("resources://BigTexture", bigTexture => { Debug.Log(bigTexture.name); });
+            mResLoader.LoadAsync<Texture2D>("resources://BigTexture", bigTexture => { Debug.Log(bigTexture.name); });
+        }
+    }
+}
