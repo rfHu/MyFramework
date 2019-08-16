8adde785c21032e761a5fad0d4d1d3cb28d91f19
RaineHuHome <2449663340@qq.com>
2019-08-16 20:09:10 +0800

    热更新Mgr

---
+++ MyFrameWork/Assets/RHFramework/Framework/ResKit/HotUpdate/HotUpdateMgr.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RHFramework
+{
+    public class HotUpdateMgr : MonoSingleton<HotUpdateMgr>
+    {
+        public bool HasNewVersionRes
+        {
+            get
+            {
+                var remoteResVersion = 1;
+                var localResVersion = 0;
+                return remoteResVersion > localResVersion;
+            }
+        }
+    }
+}
---
+++ MyFrameWork/Assets/RHFramework/Test/HotUpdateTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace RHFramework.Tests
+{
+    public class HotUpdateTest
+    {
+
+
+        [Test]
+        public void HotUpdateTestSimpleTest()
+        {
+            var needUpdate = HotUpdateMgr.Instance.HasNewVersionRes;
+
+            if (needUpdate)
+            {
+                UpdateRes();
+                Debug.Log("继续");
+            }
+            else
+            {
+                Debug.Log("继续");
+            }
+
+            Assert.IsTrue(true);
+        }
+
+        void UpdateRes()
+        {
+            Debug.Log("开始更新");
+            DownloadRes();
+            ReplaceLocalRes();
+            Debug.Log("结束更新");
+        }
+
+        private void DownloadRes()
+        {
+            Debug.Log("1.下载资源");
+        }
+
+        private static void ReplaceLocalRes()
+        {
+            Debug.Log("2.替换掉本地资源");
+        }
+    }
+}
